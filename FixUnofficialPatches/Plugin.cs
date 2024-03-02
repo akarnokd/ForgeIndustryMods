2a37d15ac637261d5f46045aa715fbbfd1bd804b
akarnokd <akarnokd@gmail.com>
2024-03-02 20:41:22 +0100

    Fix for broken achievement unlocking for 4 achievements

--- FixUnofficialPatches/Plugin.cs
+++ FixUnofficialPatches/Plugin.cs
@@ -14,6 +14,8 @@ using System;
 using TMPro;
 using System.Globalization;
 using BiteMeGames.Buildings;
+using Steamworks;
+using BepInEx.Configuration;
 
 namespace FixUnofficialPatches
 {
@@ -23,12 +25,16 @@ namespace FixUnofficialPatches
 
         static ManualLogSource logger;
 
+        static ConfigEntry<bool> achievementFix;
+
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin is loaded!");
 
             logger = Logger;
+
+            achievementFix = Config.Bind("General", "AcievementFix", false, "Enable this to apply the achievement unlock fix for 4 broken achievements.");
 
             Harmony.CreateAndPatchAll(typeof(Plugin));
         }
@@ -52,6 +58,51 @@ namespace FixUnofficialPatches
 
             var sr = ____orderTab.GetComponentInChildren<ScrollRect>();
             sr.scrollSensitivity = 5;
+        }
+
+        static readonly HashSet<string> BrokenAchievementIds = 
+        [
+            "MR_WORLD_WIDE",
+            "NEW_VENTURES",
+            "SHARPENED_STICK",
+            "WELL_ROUNDED"
+        ];
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(SteamManager), nameof(SteamManager.UnlockAchievement))]
+        static void SteamManager_UnlockAchievement(string key)
+        {
+            if (achievementFix.Value)
+            {
+                logger.LogInfo("UnlockAchievement " + key);
+                if (!BrokenAchievementIds.Contains(key))
+                {
+                    return;
+                }
+                logger.LogInfo("UnlockAchievement for broken " + key);
+                try
+                {
+
+                    SteamUserStats.RequestCurrentStats();
+
+                    logger.LogInfo("  Get SteamUserStats.Internal property value");
+                    var iSteamUserStats = AccessTools.PropertyGetter(typeof(SteamUserStats), "Internal").Invoke(null, []);
+                    
+                    logger.LogInfo("  Get ISteamUserStats.SetAchievement method.");
+                    var mSetAchievement = AccessTools.Method(iSteamUserStats.GetType(), "SetAchievement", [typeof(string)]);
+
+                    logger.LogInfo("  Invoke SetAchievement");
+                    mSetAchievement.Invoke(iSteamUserStats, [key]);
+
+                    logger.LogInfo("  Storing Stats");
+                    SteamUserStats.StoreStats();
+                }
+                catch (Exception e)
+                {
+                    logger.LogError(e);
+                }
+            }
+            logger.LogInfo("UnlockAchievement for broken " + key + " - done.");
         }
     }
 }
